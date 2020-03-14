63b9250fd697f745866724b23f91cc28d0ef5aad
zhyy2008z <zhyy2008z@outlook.com>
2020-03-14 23:14:52 +0800

    小修正2

--- ZDevTools.Rebooter/RebootController.cs
+++ ZDevTools.Rebooter/RebootController.cs
@@ -6,7 +6,11 @@ namespace ZDevTools.Rebooter
     {
         public static void RunIt(string exePath)
         {
+#if NETFRAMEWORK
             Process.Start(typeof(RebootController).Assembly.Location, "\"" + exePath + "\"" + " " + Process.GetCurrentProcess().Id);
+#elif NETCOREAPP
+            Process.Start("dotnet", "\"" + typeof(RebootController).Assembly.Location + "\" \"" + exePath + "\" " + Process.GetCurrentProcess().Id);
+#endif
         }
     }
 }
